8aa7fa9229619a567b2c92ec70be162be358980f
Rasmus Lauritsen <339185@rts-365.dk>
2020-01-10 12:29:35 +0100

    gulp boilerplate update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var fs = require("fs");
+var fs = require('fs');
 var path = require("path");
 var mkdir = require("mkdirp");
+var { exec } = require("child_process");
 
-var TEMPLATE_DIR = path.join(__dirname, "pkg");
-var MODE_0666 = parseInt("0666", 8);
+var TEMPLATE_DIR = path.join(__dirname, "pkg"); // laver sti helt fra C drevet og frem til pkg mappen i gulp-intro mappen
+
+var MODE_0666 = parseInt('0666', 8);
 
 if (!fs.existsSync("./package.json")) {
     console.log("Please run <npm init> before you run this file");
@@ -14,56 +16,48 @@ function copyFile(from, to) {
     write(to, fs.readFileSync(path.join(TEMPLATE_DIR, from), 'utf-8'));
 }
 
-function copyFileMulti(fromDir, toDir) {
-    fs.readdirSync(path.join(TEMPLATE_DIR, fromDir))
-        .forEach(function (file) {
-            if (fs.lstatSync(path.join(TEMPLATE_DIR, fromDir, file)).isFile()) {
-                copyFile(path.join(fromDir, file), path.join(toDir, file));
-            }
-        });
+function copyFiles(fromDir, toDir) {
+    fs.readdirSync(path.join(TEMPLATE_DIR, fromDir)).forEach(function (file) {
+        if (fs.lstatSync(path.join(TEMPLATE_DIR, fromDir, file)).isFile()) {
+            copyFile(path.join(fromDir, file), path.join(toDir, file));
+        }
+    });
 }
 
 function write(file, str, mode) {
-    fs.writeFileSync(file, str, { mode: mode || MODE_0666 });
-    console.log('	\x1b[36mcreate\x1b[0m  : ' + file);
+    fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
+    console.log('   \x1b[36mcreate\x1b[0m : ' + file);
 }
 
 mkdir.sync("./tasks");
 mkdir.sync("./src");
 mkdir.sync("./src/html");
-mkdir.sync("./src/layouts");
+mkdir.sync("./src/html/layouts");
 mkdir.sync("./src/scss");
 mkdir.sync("./src/js");
-mkdir.sync("./src/images");
+mkdir.sync("./src/img");
 mkdir.sync("./src/media");
 
-copyFileMulti("tasks", "./tasks");
-copyFileMulti("src/html", "./src/html");
-copyFileMulti("src/html/layouts", "./src/html/layouts");
-copyFileMulti("src/scss", "./src/scss");
-copyFileMulti("src/js", "./src/js");
-copyFileMulti("src/images", "./src/images");
-copyFileMulti("src/media", "./src/media");
+copyFiles("tasks", "./tasks");
+copyFiles("src/html", "./src/html");
+copyFiles("src/html/layouts", "./src/html/layouts");
+copyFiles("src/scss", "./src/scss");
+copyFiles("src/scss", "./src/scss");
+copyFiles("src/js", "./src/js");
+copyFiles("src/img", "./src/img");
+copyFiles("src/media", "./src/media");
 copyFile("gulpfile.js", "./gulpfile.js");
 
-// var package = require("./package");
 
-// package.devDependencies = {
-// 	"@babel/core": "^7.7.7",
-//     "@babel/preset-env": "^7.7.7",
-//     "gulp": "^4.0.2",
-//     "gulp-babel": "^8.0.0",
-//     "gulp-clean-css": "^4.2.0",
-//     "gulp-concat": "^2.6.1",
-//     "gulp-connect": "^5.7.0",
-//     "gulp-declare": "^0.3.0",
-//     "gulp-imagemin": "^6.2.0",
-//     "gulp-pug": "^4.0.1",
-//     "gulp-rename": "^2.0.0",
-//     "gulp-sass": "^4.0.2",
-//     "gulp-sourcemaps": "^2.6.5",
-//     "gulp-terser": "^1.2.0",
-//     "imagemin-jpeg-recompress": "^6.0.0"
-// };
+exec("npm i -D @babel/core @babel/preset-env gulp gulp-babel gulp-clean-css gulp-concat gulp-connect gulp-imagemin gulp-pug gulp-rename gulp-sass gulp-sourcemaps gulp-terser imagemin-jpeg-recompress", function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-// write("./package.jsaon", package);
+    console.log("Installing NPM packages. Please Wait!");
+
+    stdout.on("data", function (data) {
+        console.log(data.toString());
+    });
+});
